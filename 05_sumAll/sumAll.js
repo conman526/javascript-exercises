41953ec810bfbbb3b2da1bba1f92e4eb539b2b64
conman526 <connor.sauby@gmail.com>
2022-01-12 19:14:40 -0800

    Struggled with these, but learned a lot.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (typeof x == 'number' && typeof y == 'number') {
+        if (x <= 0 || y <= 0) {
+            return 'ERROR';
+        } else if (x < y) {
+            for (; x <= y; x++) {
+                sum += x;
+            }
+        } else if (x > y) {
+            for (; y <= x; y++) {
+                sum += y;
+            }
+        } return sum;
 
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  let temp = ((tempf - 32) * 5) / 9;
+  if (temp % 1 === 0) {
+    return temp;
+  } else {
+    return parseFloat(temp.toFixed(1));
+  }
+    
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  let temp = ((tempc * (1.8)) + 32);
+  if (temp % 1 === 0) {
+    return temp;
+  } else {
+  return parseFloat(((tempc * (1.8)) + 32).toFixed(1));
+  }
 };
 
 // Do not edit below this line
